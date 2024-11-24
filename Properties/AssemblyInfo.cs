98ddd62b699a47ce34d4597af06c3506b2c2533b
levitsky.ross <levitsky.ross@gmail.com>
2024-11-25 00:39:34 +0200

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Add_Selectors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Add_Selectors")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70d35a03-806e-40e8-9d83-c86830d0667c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/Selectors.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Assert = NUnit.Framework.Assert;
+
+namespace Selectors
+{
+    public class Selectors
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void RemoveItemFromCart()
+        {
+            //Добавляємо Cappuccino до кошика
+            driver.Navigate().GoToUrl("https://coffee-cart.app/");
+
+            //Тут необхідно знайти Cappuccino
+            //driver.FindElement(By.CssSelector()).Click();
+
+            //Тут необхідно знайти cart(1)
+            //driver.FindElement(By.CssSelector()).Click();
+
+            //Тут необхідно знайти кнопку видалення товару Cappuccino.
+            //driver.FindElement(By.XPath())Click();
+
+            // Перевіряємо, що товар більше не відображається.
+            var emptyCartTitle = driver.FindElement(By.XPath("//p[text()='No coffee, go add some.']"));
+            Assert.That(emptyCartTitle.Text, Is.EqualTo("No coffee, go add some."), "Cappuccino should be removed from the cart.");
+
+
+
+            [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
